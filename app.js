f9002318d2b84ac2bf82bd3241449a72d90d7ae4
hoanggggf <106527331+vuhoang001@users.noreply.github.com>
2024-08-20 15:47:22 +0700

    refactor

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res, next) => {
-  res.json("dang cap vu tru 11 02 ");
+  res.json("dang cap vu tru 11 02 3");
 });
 
 app.listen(3000, () => {
