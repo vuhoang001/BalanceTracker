322715a2262daff07307ece5fa7d82680e0c4125
hoanggggf <106527331+vuhoang001@users.noreply.github.com>
2024-08-23 15:55:28 +0700

    u

--- app.js
+++ app.js
@@ -2,6 +2,10 @@ const express = require("express");
 const app = express();
 require("dotenv").config();
 
+app.use("/", (req, res) => {
+  res.json("Working success");
+});
+
 require("./database/init.mongo");
 app.use("/", require("./routes/index"));
 
